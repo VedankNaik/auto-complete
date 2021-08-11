c3bddbf17460e33d50dee475d5098655faee8144
Vedank022 <vedankn01@gmail.com>
2021-08-12 01:55:46 +0530

    complete text box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [text, setText] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    const loadUsers = async () => {
+      const repsonse = await axios.get('https://reqres.in/api/users');
+      setUsers(repsonse.data.data);
+    }
+    loadUsers();
+  }, [])
+
+  const onSuggestHandler = (text) => {
+    setText(text);
+    setSuggestions([]);
+  }
+  const onChangeHandler = (text) => {
+    let matches = [];
+    if (text.length > 0) {
+      matches = users.filter(user => {
+        const regex = new RegExp(`${text}`, "gi");
+        return user.email.match(regex);
+      })
+    }
+    setSuggestions(matches);
+    setText(text);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">     
+      <input type="text" className="col-md-12 input" style={{ marginTop: 200 }}     
+      onChange={e => onChangeHandler(e.target.value)}
+      value={text}
+      onBlur={() =>{
+        setTimeout(() =>{
+          setSuggestions();
+        }, 100);
+      }}
+      />
+      {suggestions && suggestions.map((suggestion, i) =>
+        <div key={i} className="suggestion col-md-12 justify-content-md-center"
+          onClick={()=>onSuggestHandler(suggestion.email)}
+        >{suggestion.email}</div>
+      )}
     </div>
   );
 }
